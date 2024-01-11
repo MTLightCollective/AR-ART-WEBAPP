8b6630b1722cebbcf97ef33524c59cdb0223ba90
MTLightCollective <matthieu@mtlight.ca>
2024-01-11 12:46:05 -0500

    pour que le boutton disparaisse lors du play

--- src/play-button-component.js
+++ src/play-button-component.js
@@ -4,16 +4,27 @@ AFRAME.registerComponent('play-button', {
         console.log("init")
         this.clickEvent = this.clickEvent.bind(this);
         this.el.addEventListener('click', this.clickEvent);
+
+        // Get your video element
+        this.videoEl = document.getElementById('penseurvideo');
+
+        // Listen for when the video ends
+        this.videoEl.addEventListener('ended', () => {
+            this.el.setAttribute('visible', true); // Show the button again
+        });
+
     },
 
     clickEvent: function () {
         console.log("start video");
-        document.getElementById('penseurvideo').play();
+        this.videoEl.play();
+        this.el.setAttribute('visible', false); // Hide the button
     },
 
     remove: function () {
         // Code exécuté lorsque le composant est supprimé
         this.el.removeEventListener('click', this.clickEvent);
+        this.videoEl.removeEventListener('ended', this.clickEvent);
     },
     
 });
