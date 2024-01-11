b07281886d995f4f61a7e094bc705a732b0af2a3
MTLightCollective <matthieu@mtlight.ca>
2024-01-11 11:53:22 -0500

    bouton marche, via update camera component selon doc mindAR

--- src/play-button-component.js
+++ src/play-button-component.js
@@ -1,13 +1,19 @@
 AFRAME.registerComponent('play-button', {
     init: function () {
         // Code de l'initialisation du composant
+        console.log("init")
         this.clickEvent = this.clickEvent.bind(this);
         this.el.addEventListener('click', this.clickEvent);
     },
-    remove: function () {
-        // Code exécuté lorsque le composant est supprimé
-    },
+
     clickEvent: function () {
         console.log("start video");
-    }
+        document.getElementById('penseurvideo').play();
+    },
+
+    remove: function () {
+        // Code exécuté lorsque le composant est supprimé
+        this.el.removeEventListener('click', this.clickEvent);
+    },
+    
 });
