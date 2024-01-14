0d7bf7716b994f083964a009e17de21d239faa27
MTLightCollective <matthieu@mtlight.ca>
2024-01-14 09:43:05 -0500

    bouton clean

--- src/play-button-component.js
+++ src/play-button-component.js
@@ -8,15 +8,21 @@ AFRAME.registerComponent('play-button', {
         // Get your video element
         this.videoEl = document.getElementById('penseurvideo');
 
+        // Get your video Plane element
+        this.videoPlaneEl = document.getElementById('video-plane');
+
         // Listen for when the video ends
         this.videoEl.addEventListener('ended', () => {
+            console.log('Video ended!');
             this.el.setAttribute('visible', true); // Show the button again
+            this.videoPlaneEl.setAttribute('visible', false); // Hides the videoplane again
         });
 
     },
 
     clickEvent: function () {
         console.log("start video");
+        this.videoPlaneEl.setAttribute('visible', true);
         this.videoEl.play();
         this.el.setAttribute('visible', false); // Hide the button
     },
@@ -24,7 +30,6 @@ AFRAME.registerComponent('play-button', {
     remove: function () {
         // Code exécuté lorsque le composant est supprimé
         this.el.removeEventListener('click', this.clickEvent);
-        this.videoEl.removeEventListener('ended', this.clickEvent);
     },
     
 });
