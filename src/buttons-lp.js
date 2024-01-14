c9ab98b7040d289ede8eb66dc85b8418bd74970e
MTLightCollective <matthieu@mtlight.ca>
2024-01-14 11:15:55 -0500

    adding landing page and other pages

---
+++ src/buttons-lp.js
@@ -0,0 +1,15 @@
+// Event listener for the AR button
+document.getElementById('ar-button').addEventListener('click', function() {
+    window.location.href = 'index.html' ;
+  });
+  
+  // Event listener for the Map button
+  document.getElementById('map-button').addEventListener('click', function() {
+    window.location.href = 'map.html';
+  });
+  
+  // Event listener for the Info button
+  document.getElementById('info-button').addEventListener('click', function() {
+    window.location.href = 'info.html';
+  });
+  
