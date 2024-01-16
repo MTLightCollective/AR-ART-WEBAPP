7b772f4aa78d5c3c6114bcd5ace734e77f784b21
MTLightCollective <matthieu@mtlight.ca>
2024-01-16 17:48:06 -0500

    adding a new marker and video

--- src/play-button-component.js
+++ src/play-button-component.js
@@ -7,15 +7,18 @@ AFRAME.registerComponent('play-button', {
 
         // Get your video element
         this.videoEl = document.getElementById('penseurvideo');
+        this.videoE2 = document.getElementById('samuraivideo')
 
         // Get your video Plane element
         this.videoPlaneEl = document.getElementById('video-plane');
+        this.videoPlaneE2 = document.getElementById('samurai-plane');
 
         // Listen for when the video ends
         this.videoEl.addEventListener('ended', () => {
             console.log('Video ended!');
             this.el.setAttribute('visible', true); // Show the button again
             this.videoPlaneEl.setAttribute('visible', false); // Hides the videoplane again
+            this.videoPlaneE2.setAttribute('visible', false); // Hides the videoplane again
         });
 
     },
@@ -23,7 +26,9 @@ AFRAME.registerComponent('play-button', {
     clickEvent: function () {
         console.log("start video");
         this.videoEl.play();
+        this.videoE2.play()
         this.videoPlaneEl.setAttribute('visible', true);
+        this.videoPlaneE2.setAttribute('visible', true); 
         this.el.setAttribute('visible', false); // Hide the button
     },
 
